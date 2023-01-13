551b91723b3f1a7c12c305a7a5dcaa8e0ff9b409
Craig <cdevelopment010@gmail.com>
2023-01-13 21:45:47 +0000

    Feat: mobile navigation

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 
 import '../styles/nav.css'
 
 const Nav = () => {
+
+    const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width:768px").matches)
+    const [hiddenNav, setHiddenNav] = useState(isMobile);
+    const [windowWidth, setWindowWidth] = useState(0);
+    const [windowHeight, setWindowHeight] = useState(0);
+    console.log(hiddenNav);
+    
+    let resizeWindow = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+      setIsMobile(window.matchMedia("(max-width:768px").matches);
+      
+    }
+
+    useEffect(()=> {
+      const timeout = setTimeout(() => {
+        resizeWindow(); 
+        window.addEventListener("resize", resizeWindow); 
+
+      }, 500)
+      return () => {
+        window.removeEventListener("resize", resizeWindow)
+        clearTimeout(timeout);
+      }
+    }, [windowWidth, windowHeight])
+
+    useEffect(() => {
+      setHiddenNav(isMobile)
+    }, [isMobile])
+
+    const toggleNav = () => {
+      setHiddenNav(!hiddenNav);
+    }
+
     return (
       <div className="nav-container">
         <Link to="/" className="logo">
@@ -10,22 +46,34 @@ const Nav = () => {
             Computer <br></br> Store
           </h2>
         </Link>
-        <ul className="nav">
-            <li className="nav-items">
-                <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-items">
-                <Link to="/shop" className="nav-link">Shop</Link>
-            </li>
-        </ul>
-        <div className="icons">
-          <div>
-            SEARCH
+
+        {isMobile?
+          <i className="fa-solid fa-bars" onClick={toggleNav}></i>
+          : null
+        }
+
+          <div className={`${isMobile ? "mobile-nav" : ""} ${hiddenNav ? "" : "active"}`}>
+            <ul className={`nav`}>
+                <li className="nav-items">
+                    <Link to="/" className="nav-link" onClick={() => isMobile? setHiddenNav(true):null}>Home</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/shop" className="nav-link" onClick={() => isMobile? setHiddenNav(true):null}>Shop</Link>
+                </li>
+            </ul>
+            <div className="icons">
+              <div>
+              <i className="fa-solid fa-magnifying-glass"></i>
+              </div>
+              <div>
+                <Link to="/basket" className="inherit-color" onClick={() => isMobile? setHiddenNav(true):null}>
+                  <i className="fa-solid fa-basket-shopping"></i>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div>
-            <Link to="/basket">BASKET</Link>
-          </div>
-        </div>
+
+        
       </div>
     );
   }
