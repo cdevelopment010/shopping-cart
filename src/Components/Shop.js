81733f967e250e99a0457d6c585bc09ff6069b31
cdevelopment010 <cdevelopment010@gmail.com>
2023-01-19 19:14:57 +0000

    Feat: shop nav toggle on mobile

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import ItemCard from "./ItemCard";
 import '../styles/shop.css'
 
 const Shop = () => {
+
+    const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width:768px").matches)
+    const [hiddenComponents, setHiddencomponents] = useState(isMobile);
+    
+    
+    const hideComp = () => {
+      setHiddencomponents(!hiddenComponents)
+    }
+
+
     return (
       <div className="shop-container">
         
         <div className="grid"> 
           <div className="sidebar">
-            <span>Components</span>
+            <div>
+              <span onClick={hideComp}>Components</span>
+              <ul className={`${hiddenComponents ? 'hidden' : ''}`}>
+                <li>Motherboard</li>
+                <li>Motherboard</li>
+                <li>Motherboard</li>
+                <li>Motherboard</li>
+                <li>Motherboard</li>
+              </ul>
+            </div>
             <span>Accessories</span>
             <span>Pre-builds</span>
           </div>
