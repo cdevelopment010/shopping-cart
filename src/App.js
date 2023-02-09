601c5535845cd3cb29c0948d3db1ebdf0a29a3f8
cdevelopment010 <cdevelopment010@gmail.com>
2023-02-09 18:10:57 +0000

    Feat: Desktop basket slider

--- src/App.js
+++ src/App.js
@@ -83,13 +83,13 @@ const App = () => {
 
   return (
     <div className="grid grid-main">
-      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
-        <Nav isMobile={isMobile} basketCount={basketCount}/>
+      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}> 
+        <Nav isMobile={isMobile} basketCount={basketCount} basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop isMobile={isMobile} addToBasket={addToBasket} removeFromBasket={removeFromBasket} basket={basket}/>} />
           <Route path="/shop/:id" element={<ItemDetail addToBasket={addToBasket} removeFromBasket={removeFromBasket} basket={basket}/>} />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout" element={<Checkout basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket}/>} />
           <Route path="/basket" element={<Checkout title="Basket" basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket}/>} />
 
         </Routes>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,39 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ItemCard from './ItemCard';
 
 
 import '../styles/nav.css'
 
-const Nav = ({ isMobile, basketCount }) => {
+const Nav = ({ isMobile, basketCount, basket, addToBasket, removeFromBasket }) => {
 
 
     const [hiddenNav, setHiddenNav] = useState(isMobile);
+    const [desktopBasketVisible, setDesktopBasketVisible] = useState(false);
+    const [total, setTotal] = useState(0); 
 
     useEffect(() => {
       setHiddenNav(isMobile)
     }, [isMobile])
 
+    useEffect(()=> {
+      calculateTotal()
+    },[basket])
+
     const toggleNav = () => {
       setHiddenNav(!hiddenNav);
+    }
+
+    const desktopBasket = () => {
+      setDesktopBasketVisible(!desktopBasketVisible);
+    }
+
+    const calculateTotal = () => {
+      let sum = basket.reduce((prev, curr) => {
+        return prev + (curr.price * curr.count); 
+      }, 0)
+      console.log("sum", sum); 
+      setTotal(sum); 
     }
 
     return (
@@ -44,15 +63,41 @@ const Nav = ({ isMobile, basketCount }) => {
               <i className="fa-solid fa-magnifying-glass"></i>
               </div>
               <div>
-                <Link to="/basket" className="inherit-color" onClick={() => isMobile? setHiddenNav(true):null}>
-                  <i className="fa-solid fa-basket-shopping position-relative">
+                {isMobile &&
+                  <Link to="/basket" className="inherit-color" onClick={() => isMobile? setHiddenNav(true):null}>
+                    <i className="fa-solid fa-basket-shopping position-relative">
+                      <span className="basket-count">{basketCount}</span>
+                    </i>
+                  </Link>
+                }
+
+                {!isMobile && 
+                  <i className="fa-solid fa-basket-shopping position-relative" onClick={desktopBasket}>
                     <span className="basket-count">{basketCount}</span>
                   </i>
-                </Link>
+
+                }
               </div>
             </div>
           </div>
 
+          <div className={`desktop-basket ${desktopBasketVisible ? 'visible' : 'invisible'} ${isMobile ? 'd-none' : ''}`}>
+                <i className={`fa-solid fa-times ${desktopBasketVisible ? '' : 'd-none' }`} onClick={desktopBasket}></i>
+                <div className="grid mt-5 mb-5 justify-content-center gap-3">
+                  <h3 className="text-center">Basket</h3>
+                  {basket.length > 0 && basket.map((b,ind) => {
+                    return <ItemCard data={b} key={`item-${b.id}`} addToBasket={addToBasket} basket={basket} removeFromBasket={removeFromBasket}/>
+                  })}
+
+                  {/* Total */}
+                  <div className="total mb-2">
+                    Total: £<span>{total}</span>
+                  </div>
+
+                  {/* checkout button */}
+                  <Link to="/checkout" onClick={desktopBasket}><button>Checkout</button></Link>
+                </div>
+          </div>
         
       </div>
     );
